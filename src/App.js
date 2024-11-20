c77c053c8395536180b89ed29ccfdafcac3edbf4
arushpandey2006 <ggtxarush@gmail.com>
2024-11-21 01:09:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Page from './component/page.jsx'
+import Pop from './component/Pop/pop.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Pop/> */}
+      <Page/>
+    </div>
+  );
+}
+
+export default App;
